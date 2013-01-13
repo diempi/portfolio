caaaf95a2baa3871cdc06c3decde4e8d57772043
Didier DIEMPI <dev@diempi.be>
2013-01-13 03:19:59 +0100

    Ajout de la barre d'adminstration lorsqu'on est connecté

--- footer.php
+++ footer.php
@@ -11,10 +11,11 @@
         <script>window.jQuery || document.write('<script src="http://static.diempi.be/jquery.min.js"><\/script>')</script>
 
         <script>
-            var _gaq=[['_setAccount','UA-7257900-3'],['_trackPageview']];
+            var _gaq=[['_setAccount','UA-XXXXX-X'],['_trackPageview']];
             (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
             g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
             s.parentNode.insertBefore(g,s)}(document,'script'));
         </script>
+            <?php wp_footer(); ?>
     </body>
 </html>
--- header.php
+++ header.php
@@ -3,6 +3,7 @@
 <!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
 <!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
 <!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+    <?php wp_head(); ?>
     <head>
         <meta charset="<?php bloginfo('charset'); ?>">
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
@@ -11,8 +12,8 @@
         <meta name="viewport" content="width=device-width">
         <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,800' rel='stylesheet' type='text/css'>
         <link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
-        <script src="//cdnjs.cloudflare.com/ajax/libs/modernizr/2.6.2/modernizr.min.js"></script>
-        <!--script src="http://static.diempi.be/modernizr.min.js" type="text/javascript"></script-->
+
+        <script src="http://static.diempi.be/modernizr.min.js" type="text/javascript"></script>
     </head>
     <body>
         <!--[if lt IE 7]>
@@ -22,7 +23,7 @@
         <div class="header-container">
             <header class="wrapper clearfix">
                 <h1 class="title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home" ><?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?> <span><?php bloginfo( 'description' ); ?></span></a></h1>
-				<?php wp_nav_menu(array('container' => 'ul', 'menu_id'=>'nav')); ?>
+				<?php wp_nav_menu( array('menu' => 'mon-menu' )); ?>
             </header>
         </div>
 
